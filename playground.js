133b944f49151608bab765e192fd5dbe928e2af7
davehillnz <47799907+davehillnz@users.noreply.github.com>
2023-02-01 12:22:24 +1300

    Debugging

--- playground.js
+++ playground.js
@@ -1,11 +1,12 @@
 $( function () {
     $( "#picView" ).click( function (){
-        alert ( "here" );
+        
         if( $( "html" ).hasClass ( "showPic" ) ){
             $( "html" ).removeClass( "showPic" );
         }
         else{
             $( "html" ).addClass( "showPic" );
+            alert ( "here" );
         }
         
     })
